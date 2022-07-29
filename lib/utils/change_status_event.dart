db88bf0d0f5a96e78a5ebc308028c0b22b9a4ffe
Uarlley <uarleynascimentoamorim@gmail.com>
2022-07-29 18:22:04 +0000

    fixed some api problems

--- lib/utils/change_status_event.dart
+++ lib/utils/change_status_event.dart
@@ -1,6 +1,7 @@
 import 'package:os_controller/utils/task.dart';
 
 class ChangeStatusEvent {
+  // ignore: non_constant_identifier_names
   final Task updated_task;
 
   ChangeStatusEvent(this.updated_task);
